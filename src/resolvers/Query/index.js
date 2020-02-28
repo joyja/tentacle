b9aa96eb2d93b54e8edab4f13cbec562b0378e16
joyja <joyja@latitude1>
2020-02-28 22:46:31 +0000

    added user query

--- src/resolvers/Query/index.js
+++ src/resolvers/Query/index.js
@@ -1,8 +1,10 @@
+const user = require('./user')
 const tag = require('./tag')
 const device = require('./device')
 const service = require('./service')
 
 module.exports = {
+  ...user,
   ...tag,
   ...device,
   ...service
---
+++ src/resolvers/Query/user.js
@@ -0,0 +1,9 @@
+const { User } = require('../../auth')
+
+async function user(root, args, context, info) {
+  return User.getUserFromContext(context)
+}
+
+module.exports = {
+  user
+}
