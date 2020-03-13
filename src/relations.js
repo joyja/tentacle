e942125de4bce39e0782cc167735125292f32273
joyja <joyja@latitude1>
2020-03-13 22:36:29 +0000

    removed logging

--- src/relations.js
+++ src/relations.js
@@ -345,8 +345,6 @@ Mqtt.prototype.publishHistory = async function() {
   let params = historyToPublish.map((record) => {
     return record.id
   })
-  console.log(sql)
-  console.log(params)
   await this.constructor.executeUpdate(sql, params)
   sql = `DELETE FROM mqttHistory 
     WHERE EXISTS 
