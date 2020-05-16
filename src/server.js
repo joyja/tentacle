a619252bd1da47aba6d5907171b695fed5a72627
James Joy <joyja@jarautomation.io>
2020-05-16 16:23:40 -0700

    incremented database version

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const { executeQuery } = require('./database/model')
 const fs = require('fs')
 const logger = require('./logger')
 
-const desiredUserVersion = 5
+const desiredUserVersion = 6
 
 let db = undefined
 let httpServer = undefined
